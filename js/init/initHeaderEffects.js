17ea2539654bb8f8299ad47b540cfe297b8357ab
sjd23dev <SJD23Dev@proton.me>
2025-05-03 19:23:17 -0500

    Switched from pastel palette to cyberpunk, and added new characters to asciiCharacters

--- js/init/initHeaderEffects.js
+++ js/init/initHeaderEffects.js
@@ -2,13 +2,19 @@ import { startHeaderDissolveEffect } from "../effects/headerDissolve.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     const dissolveTextElement = document.getElementById("header-title");
-    const asciiCharacters = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+    const asciiCharacters =
+        "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~•●○◯◌♦◇";
     const colors = [
-        "#F5A9A9", "#F5BCA9", "#F5D0A9", "#F3E2A9", "#F2F5A9", "#E1F5A9",
-        "#D0F5A9", "#BCF5A9", "#A9F5A9", "#A9F5BC", "#A9F5D0", "#A9F5E1",
-        "#A9F5F2", "#A9E2F3", "#A9D0F5", "#A9BCF5", "#A9A9F5", "#BCA9F5",
-        "#D0A9F5", "#E2A9F3", "#F5A9F2", "#F5A9E1", "#F5A9D0", "#F5A9BC"
+        "#08f7fe", "#00f0ff", "#00e5ff", "#00d2ff", "#00c3ff",
+        "#ff2d95", "#ff00c8", "#ff4da6", "#ff66cc", "#ff9ff3",
+        "#f368e0", "#de5dff", "#c04fff", "#a042f5", "#7d5fff",
+        "#5f27cd", "#7a00ff", "#a100ff", "#c300ff", "#ff66ff",
+        "#f5d300", "#fff700", "#ffff00", "#ffea00", "#cfff04",
+        "#00ff9f", "#39ff14", "#1dd1a1", "#00d2d3", "#00ffdd",
+        "#ff9966", "#ff6f61", "#ff3131", "#ee5253", "#ff003c",
+        "#2e86de", "#0066ff", "#0033ff", "#341f97", "#1a1aff"
     ];
+
 
     startHeaderDissolveEffect(dissolveTextElement, asciiCharacters, colors);
 });
